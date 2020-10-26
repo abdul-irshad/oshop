0e6cab4d234fe4bfc17b532a7db1a0c01557009e
Abdul Irshad <er.abdulirshad@gmail.com>
2020-10-26 19:33:44 +0530

    This is the new message

--- src/app/Admin/product-form/product-form.component.ts
+++ src/app/Admin/product-form/product-form.component.ts
@@ -1,25 +1,18 @@
 import { OshopServiceService } from './../../Shared/oshop-service.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-form',
-  templateUrl:'./product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css'],
 })
 export class ProductFormComponent implements OnInit {
-  categories$;
+  categories$:Observable<any>; 
 
-  constructor(private service:OshopServiceService) { }
+  constructor(private service: OshopServiceService) {}
 
   ngOnInit() {
-    this.categories$= this.service.getCategory()
-    //console.log(this.categories$);
-    //.subscribe(response=>{
-    //   console.log(response);
-    //   this.categories$=response;
-    //   console.log(this.categories$);
-
-    // });
+  this.categories$= this.service.getCategory()
   }
-
 }
